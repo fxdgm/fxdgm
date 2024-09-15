2e3be68d798a52b13f5bcbf7e29e2a9c31a2c806
Jan H <Jan.Habscheid@web.de>
2024-09-15 18:11:08 +0200

    Updated docs in ci for proper integration with source code docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = 'September, 2024'
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join("..", "../src")))
-# sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
