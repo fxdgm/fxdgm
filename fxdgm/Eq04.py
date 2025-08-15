da7517e2d27b37a1597880e97dbd848b8f0414ab
JanHab <Jan.Habscheid@web.de>
2025-08-15 16:06:47 +0200

    Add comment about implementation shortcuts due to equilibrium

--- fxdgm/Eq04.py
+++ fxdgm/Eq04.py
@@ -172,6 +172,7 @@ def solve_System_4eq(phi_left:float, phi_right:float, p_right:float, z_A:float, 
             return (z_C * y_C + z_A * y_A)
         
         # Diffusion fluxes for species A and C
+        # The gradient of the chemical potential of the neutral solvent vanishes as we are in equilibrium 
         def J_A(y_A, y_C, phi, p):
             return grad(ln(y_A) + a2 * (p - 1) * (solvation + 1) + z_A * phi)
             # return grad(ln(y_A) + a2 * (p - 1) - solvation * ln(1-y_A-y_C) + z_A * phi)
